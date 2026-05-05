8b5df3172d96600b53c1b35bb71a18b519233049
dhunganasalina43 <dhunganasalina43@gmail.com>
2026-05-05 09:28:51 +0545

    frontend created

---
+++ app/types.ts
@@ -0,0 +1,11 @@
+export type QueryItem = {
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  query: string;
+};
+
+export const BACKEND_API_URL =
+  "https://salina-24-a-backend-eneyc8cscndvadfx.centralindia-01.azurewebsites.net/queries";
+
+export const APP_API_URL = "/api/queries";
